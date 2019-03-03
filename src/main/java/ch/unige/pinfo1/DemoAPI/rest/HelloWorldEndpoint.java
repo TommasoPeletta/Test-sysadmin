c3a36aca21468ed3f534bd50aa93c2fedbc5e764
TommasoPeletta <you@example.com>
2019-03-03 12:01:03 +0100

    travis marche

---
+++ src/main/java/ch/unige/pinfo1/DemoAPI/rest/HelloWorldEndpoint.java
@@ -0,0 +1,29 @@
+package ch.unige.pinfo1.DemoAPI.rest;
+
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+	@GET
+	@Produces("text/html")
+	public String doGet() {
+		return "<h1>Hello from Thorntail!</h1>";
+	}
+	
+	@GET
+	@Produces("text/html")
+	@Path("/plusage/{name}")
+	public String hello_name(@PathParam("name") String name,
+			@DefaultValue("25") @QueryParam("age") int age) {
+		return "helloh "+name+"! Nice to see you! You are " + age + " years old";
+	}
+}
